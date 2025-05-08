165b547e65260e5e01c1414c97e189847c520657
AlbertooR04 <alberto.romerolucas@gmail.com>
2025-05-08 12:10:04 +0200

    Update pre_fastqc.sh
    nombre

--- SRV_01/01-pre_fastqc/pre_fastqc.sh
+++ SRV_01/01-pre_fastqc/pre_fastqc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Script name: pre_fastqc
-#Author: Jaime Salama García
+#Author: Jaime Salama García & Alberto Romero Lucas
 #Date: 08/05/2025
 #Purpose: using fastqc and multiqc for checking quality BEFORE trimming and filtering
 #DEPENDENCES: fastqc and multiqc
