9a5b79584e0a7c6a87983ad43b50ed968804049b
AlbertooR04 <alberto.romerolucas@gmail.com>
2025-05-08 12:09:22 +0200

    Update Import_raw_data.sh
    nombre

--- SRV_01/00-raw_data/Import_raw_data.sh
+++ SRV_01/00-raw_data/Import_raw_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Script name: import samples from NCBI
-#Author: Jaime Salama García
+#Author: Jaime Salama García & Alberto Romero Lucas
 #Date: 08/05/2025
 #Purpose: using sra-toolkit import raw data with accesion number
 #DEPENDENCES: sra-tools
