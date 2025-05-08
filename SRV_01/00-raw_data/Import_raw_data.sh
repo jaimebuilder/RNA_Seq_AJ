80aefee930f75094a6eef4cac6086053d6376432
jaimebuilder <162809133+jaimebuilder@users.noreply.github.com>
2025-05-08 12:06:33 +0200

    creados los scripts necesarios, aún hay que hacerlos

---
+++ SRV_01/00-raw_data/Import_raw_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Script name: import samples from NCBI
+#Author: Jaime Salama García
+#Date: 08/05/2025
+#Purpose: using sra-toolkit import raw data with accesion number
+#DEPENDENCES: sra-tools
---
+++ SRV_01/03-post_fastqc/post_fastqc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Script name: post_fastqc
+#Author: Jaime Salama García & Alberto Romero Lucas
+#Date: 08/05/2025
+#Purpose: using fastqc and multiqc for checking quality AFTER trimming and filtering
+#DEPENDENCES: fastqc and multiqc
