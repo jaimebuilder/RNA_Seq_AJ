c4639c952acf7b875f5f35b85bb9c010981a95fe
AlbertooR04 <alberto.romerolucas@gmail.com>
2025-05-18 14:51:00 +0200

    Update Import_raw_data.sh

--- SRV_01/00-raw_data/Import_raw_data.sh
+++ SRV_01/00-raw_data/Import_raw_data.sh
@@ -5,12 +5,12 @@
 #Purpose: using sra-toolkit import raw data with accesion number
 #DEPENDENCES: sra-tools
 #readonly VERSION="1.0.0"
-#Usage: ./Import_raw_data.sh -f SRA file 
+#Usage: ./Import_raw_data.sh -f SRR file 
 # El script requiere estos argumentos, con las siguientes flags:
 #1. -f SRA_file.txt File that contains the SRA accessions of the samples, one per line.
 #-h displays help
 #v displays version
-help_text="Usage: ./Import_raw_data.sh -f SRA file"
+help_text="Usage: ./Import_raw_data.sh -f SRR_file"
 
 #Parssing arguments
 while getopts "hvf:" opt; do
@@ -19,14 +19,14 @@ while getopts "hvf:" opt; do
 			exit 0;;
        	v) echo "Version: $VERSION"  # Display version info
        		exit 0 ;;
-        f) SRA_file="$OPTARG"
+        f) SRR_file="$OPTARG"
     	?) echo "Invalid option or missing argument: $help_text" >&2
        		exit 1 ;;
 	esac
 done
 
 #SRA_file.txt verification
-if ! [[ -s $SRA_file ]]; then
+if ! [[ -s $SRR_file ]]; then
         echo "error in Genome Annotation file: $SRA_file not found or is empty" >&2
         exit 2
 fi
@@ -39,8 +39,8 @@ if ! [[ -e ./logs ]]; then
 
 {
 while IFS= read -r SRA; do
-    prefetch $SRA
-    fasterq-dump $SRA --split-files --threads 20 -O ./
+    prefetch $SRR
+    fasterq-dump $SRR --split-files --threads 20 -O ./
     gzip ./*.fastq
-done < $SRA_file
+done < $SRR_file
 } >> ./logs/raw_data_output.log 2>> ./logs/raw_data_alignment_error.log
