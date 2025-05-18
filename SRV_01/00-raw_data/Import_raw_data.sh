0c88b58d2000644471019c2441fd73f9d062aa1d
AlbertooR04 <alberto.romerolucas@gmail.com>
2025-05-18 20:57:00 +0200

    GTF

--- SRV_01/00-raw_data/Import_raw_data.sh
+++ SRV_01/00-raw_data/Import_raw_data.sh
@@ -2,11 +2,12 @@
 #Script name: import samples from NCBI
 #Author: Jaime Salama García & Alberto Romero Lucas
 #Date: 08/05/2025
-#Purpose: using sra-toolkit import raw data with accesion number
+#Purpose: using sra-toolkit import raw data with accesion number and import GTF_file
 #DEPENDENCES: sra-tools
 #Usage: ./Import_raw_data.sh -f SRA file 
 # El script requiere estos argumentos, con las siguientes flags:
 #1. -f SRA_file.txt File that contains the SRA accessions of the samples, one per line.
+#2. -g URL from the GTF file that we will use
 #-h displays help
 #v displays version
 
@@ -15,10 +16,11 @@
 while getopts "hvf:" opt; do
 	case $opt in
     	h) echo $help_text
-			exit 0;;
+		exit 0;;
        	v) echo "Version: $VERSION"  # Display version info
        		exit 0 ;;
-        f) SRA_file="$OPTARG"
+        f) SRA_file="$OPTARG" ;;
+        g) GTF_URL="$OPTARG" ;;
     	?) echo "Invalid option or missing argument: $help_text" >&2
        		exit 1 ;;
 	esac
@@ -34,8 +36,15 @@ fi
 if ! [[ -e ./logs ]]; then
                 echo "./logs directory does not exists, creating..."
                 mkdir ./logs
-        fi
+fi
+#Obtaining GTF_file
+if ! [[ -e ../genome_files/*.gtf ]]; then
+                echo "GTF_file does not exit. Downloading..."
+                wget -P ../genome_files/ $GTF_URL 
+fi
 
+
+#Obtaining fastq.gz files from NCBI using sra-tools
 {
 while IFS= read -r SRA; do
     prefetch $SRA
