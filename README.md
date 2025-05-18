a27eaea3aa73112577dcb45d63947bed5cea6a1c
jaimebuilder <162809133+jaimebuilder@users.noreply.github.com>
2025-05-18 18:58:19 +0200

    post_fastqc y detalles

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Info incompleta:
 Conda env creation:
 conda create -n RNA_SEQ
 
-mamba install sra-tools fastqc multiqc pigz
+mamba install sra-tools fastqc multiqc pigz fastp
 
 
 #ACLARAR PQ USAMOS PIGZ (multiples hilos)
--- SRV_01/03-post_fastqc/post_fastqc.sh
+++ SRV_01/03-post_fastqc/post_fastqc.sh
@@ -4,3 +4,47 @@
 #Date: 08/05/2025
 #Purpose: using fastqc and multiqc for checking quality AFTER trimming and filtering
 #DEPENDENCES: fastqc and multiqc
+readonly VERSION="1.0.0"
+#-i input directory
+#-o output directory
+#-h displays help
+#-v displays version
+readonly help_text="Usage: $(basename $0) -i input_dir -o out_dir "
+
+#Parssing arguments
+while getopts "hvi:o:" opt; do
+	case $opt in
+    	h) echo $help_text #Displays help
+			exit 0;;
+       	v) echo "Version: $VERSION"  # Display version info
+       		exit 0 ;;
+    	i) inputDir="$OPTARG" ;;
+		o) outputDir="$OPTARG" ;;
+    	*) echo "Invalid option or missing argument: $help_text" >&2
+       		exit 1 ;;
+	esac
+done
+
+if [[ -z "$inputDir" || -z $outputDir ]]; then
+        echo "Invalid option or missing argument: $help_text" >&2
+        exit 1
+fi
+
+if ! [[ -e $outputDir/logs ]]; then
+                echo "$outputDir/logs directory does not exists, creating..."
+                mkdir $outputDir/logs
+				echo "$outputDir/logs created"
+fi
+
+if ! [[ -e $outputDir/fastqc ]]; then
+                echo "$outputDir/fastqc directory does not exists, creating..."
+                mkdir $outputDir/fastqc
+				echo "$outputDir/fastqc created"
+fi
+
+{
+
+fastqc ${inputDir}/*_1.fastq.gz ${inputDir}/*_2.fastq.gz -o ${outputDir}/fastqc -t 20   #Call fastqc with both forward and reverse samples, -o for output directory and -t for the threads fastqc could use.
+multiqc ${outputDir}/fastqc -o ${outputDir}/multiqc     # Call multiqc with the output of fastqc, -o is for output directory.
+
+} 2>> $outputDir/logs/post_fastqc_error.log >> $outputDir/logs/post_fastqc.log 
